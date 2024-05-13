0a84ae184f5efd8c572c7f6f2d85fcfb78ab865f
AyaAl-wahidi <ayanalwahidi@gmail.com>
2024-05-13 11:17:25 +0300

    submit lab 17

--- my-app/src/components/header/Header.js
+++ my-app/src/components/header/Header.js
@@ -1,6 +1,12 @@
+import './Header.css';
+import Navbar from '../Navbar/Navbar';
+
 function Header() {
     return (
+      <>
       <h1>Aya Travel Destination App </h1>
+      <Navbar />
+      </>
     );
   }
   
--- my-app/src/components/home/Home.js
+++ my-app/src/components/home/Home.js
@@ -1,18 +1,13 @@
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import Tours from '../tours/Tours';
-
-const tourData = require('../../data/db.json');
+import './Home.css'
 
 function Home() {
     return (
         <>
             <Header />
-            <div>
-                {tourData.map((element) => (
-                    <Tours city_id={element.id} city_name={element.name} city_image={element.image} />
-                ))}
-            </div>
+            <Tours />
             <Footer />
         </>
     );
